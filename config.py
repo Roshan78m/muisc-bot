dc1b238622aef7b1fcd9bce8d9f2b3336285cbf9
Zeno79 <144168045+Zeno79@users.noreply.github.com>
2024-04-06 21:24:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Paradox_bots")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+mKXIX38_UpMxOTg1")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Paradox_anime_gang")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
